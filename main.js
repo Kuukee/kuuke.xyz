a1084fbdd1a7eed4af048ddabb7091831bde163c
Kuukee <lf4122@gmail.com>
2022-06-05 01:52:23 +0200

    removed varheight

--- main.js
+++
@@ -1,7 +0,0 @@
-//Height for iphones (fuck iphones)
-const appHeight = () => {
-    const doc = document.documentElement
-    doc.style.setProperty(" — app-height", `${window.innerHeight}px`)
-   }
-   window.addEventListener("resize", appHeight)
-   appHeight()
