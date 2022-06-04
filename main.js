6ec27d5e8a5ee22b32412b71224e1da7530639de
Kuukee <lf4122@gmail.com>
2022-06-05 01:31:09 +0200

    Added Projects page together with status

--- main.js
+++ main.js
@@ -1,36 +1,3 @@
-//I'm using this js for the index.html js, I dunno if this is commonplace but this seem like a nice way to divide stuff in its desired pages
-
-
-// When the user scrolls down 20px from the top of the document, show the button
-//And gets the button ID lol
-var mybutton = document.getElementById('myBtn');
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-    if (window.scrollY > 200 || window.scrollY > 200) {
-        mybutton.style.display = "block";
-    } else {
-        mybutton.style.display = "none";
-    }
-}
-
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-    window.scrollTo({top: 0, behavior: 'smooth'});
-}
-
-// General scrolling
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
-
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
-    });
-});
-
-
 //Height for iphones (fuck iphones)
 const appHeight = () => {
     const doc = document.documentElement
---
+++ projects.js
@@ -0,0 +1,24 @@
+function secondsToHms(d) {
+    d = Number(d);
+    var h = Math.floor(d / 3600);
+    var m = Math.floor(d % 3600 / 60);
+    var s = Math.floor(d % 3600 % 60);
+
+    var hDisplay = h > 0 ? h + (h == 1 ? " hour, " : " hours, ") : "";
+    var mDisplay = m > 0 ? m + (m == 1 ? " minute, " : " minutes, ") : "";
+    var sDisplay = s > 0 ? s + (s == 1 ? " second" : " seconds") : "";
+    return hDisplay + mDisplay + sDisplay; 
+}
+
+setInterval(async () => {
+    try {
+      const resp = await fetch("http://localhost:8080");
+      const uptime = await resp.text();
+      // Optionally transform into minutes for extra points
+      document.getElementById("status").textContent = "Bot has been online for: " + secondsToHms(uptime);
+    } catch (error) {
+      document.getElementById("status").textContent = "Bot is currently offline :(";
+    }
+    
+  }, 1000);
+
