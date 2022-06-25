1b3c627ad219ff7374d935b0f7c71ede8854f48e
Kuukee <lf4122@gmail.com>
2022-06-25 17:58:12 +0200

    added days for the status

--- projects.js
+++ projects.js
@@ -1,13 +1,15 @@
-function secondsToHms(d) {
-    d = Number(d);
-    var h = Math.floor(d / 3600);
-    var m = Math.floor(d % 3600 / 60);
-    var s = Math.floor(d % 3600 % 60);
+function secondsTodHms(uptime) {
+    uptime = Number(uptime);
+    var d = Math.floor(uptime / 86400);
+    var h = Math.floor(uptime % 86400 / 3600);
+    var m = Math.floor(uptime % 3600 / 60);
+    var s = Math.floor(uptime % 60);
 
+    var dDisplay = d > 0 ? d + (d == 1 ? " day, " : " days, ") : "";
     var hDisplay = h > 0 ? h + (h == 1 ? " hour, " : " hours, ") : "";
     var mDisplay = m > 0 ? m + (m == 1 ? " minute, " : " minutes, ") : "";
     var sDisplay = s > 0 ? s + (s == 1 ? " second" : " seconds") : "";
-    return hDisplay + mDisplay + sDisplay; 
+    return dDisplay + hDisplay + mDisplay + sDisplay; 
 }
 
 setInterval(async () => {
@@ -16,7 +18,7 @@ setInterval(async () => {
       console.log(resp);
       const uptime = await resp.text();
       // Optionally transform into minutes for extra points
-      document.getElementById("status").textContent = "Bot has been online for: " + secondsToHms(uptime);
+      document.getElementById("status").textContent = "Bot has been online for: " + secondsTodHms(uptime);
     } catch (error) {
       document.getElementById("status").textContent = "Bot is currently offline :(";
     }
